97a55342098e1ab3107d728647869c2afc777cae
Matthew Chatham <matthew.a.chatham@gmail.com>
2018-01-26 19:50:33 -0800

    get_kaggle_data documentation

--- toxic/common/data.py
+++ toxic/common/data.py
@@ -8,7 +8,18 @@ January 26
 import pandas as pd
 
 def get_kaggle_data(file='train', verbose=False):
-    """Gets the data from Google Cloud Storage"""
+    """
+    Get Kaggle data.
+    
+    Parameters
+    ----------
+    file : str, 'train|test' to get the corresponding data (default 'train')
+    verbose : bool, True to see progress messages from BigQuery (default False)
+    
+    Returns
+    ----------
+    res : a pandas dataframe holding the train/test data
+    """
     if file not in ['train', 'test']: raise ValueError('file must be train|test')
     
     query = """
